7d0a69f71cd614b34ff63f95eaea1ddcb459255f
John Paul Atencio <johnpaultnc26@gmail.com>
2025-06-13 21:27:00 +0800

    added float switch

--- firmware/include/config.h
+++ firmware/include/config.h
@@ -5,4 +5,7 @@
 #define DHT_TYPE DHT11 // Type of DHT sensor (DHT11, DHT22, etc.)
 
 #define ONE_WIRE_BUS 23 // GPIO pin for OneWire bus (if using OneWire sensors)
+
+//Float switch configuration
+#define FLOAT_SWITCH_PIN 25 // GPIO pin connected to the float switch
 #endif
